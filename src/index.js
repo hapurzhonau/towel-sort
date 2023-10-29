865f7691a9bcbbbbbde4bd9764cf91fc9a65cd57
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2023-10-30 01:13:15 +0300

    feat: complete task (Mon, Oct 30, 2023 01:13:15 AM)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  let res = [];
+  if (!matrix) {
+    return [];
+  } else {
+    matrix.map((el, ind) => {
+      if (ind % 2 === 0 || ind === 0) {
+        el.map(el2 => res.push(el2))
+      } else {
+        el.reverse().map(el3 => res.push(el3))
+      }
+    })
+  };
+  return res;
 }
